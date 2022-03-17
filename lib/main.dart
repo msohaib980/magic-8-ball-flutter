d8368bc6c9acb65086cc01a550a6a1a52d16b174
MSKhan <msohaib980@gmail.com>
2022-03-17 14:31:06 +0500

    completed project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.black,
+          appBar: AppBar(
+            title: Text('8 Ball life predectation'),
+            backgroundColor: Colors.teal,
+          ),
+          body: eightBall(),
+        ),
       ),
     );
+
+class eightBall extends StatefulWidget {
+  @override
+  _eightBallState createState() => _eightBallState();
+}
+
+class _eightBallState extends State<eightBall> {
+  int tartCard = 2;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.all(80),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    tartCard = Random().nextInt(5) + 1;
+                    print(' this is number $tartCard');
+                  });
+                },
+                child: Image.asset('images/ball$tartCard.png'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
